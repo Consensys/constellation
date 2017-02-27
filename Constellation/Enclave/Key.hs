2fec4e3e26ac60a367e6f21c5e7f11b006a360de
Brian Schroeder <bts@gmail.com>
2017-02-27 12:07:54 -0500

    Extract util maybeToEitherT

--- Constellation/Enclave/Key.hs
+++ Constellation/Enclave/Key.hs
@@ -6,14 +6,14 @@ module Constellation.Enclave.Key where
 
 import Prelude (putStrLn)
 import ClassyPrelude hiding (hash, putStrLn)
-import Control.Monad.Trans.Either (EitherT(EitherT), hoistEither, runEitherT)
+import Control.Monad.Trans.Either (EitherT(EitherT), runEitherT)
 import qualified Data.Aeson as AE
 import qualified Crypto.Saltine.Class as S
 import qualified Crypto.Saltine.Core.Box as Box
 
 import Constellation.Enclave.Types (PublicKey(PublicKey), mkPublicKey)
 import Constellation.Util.ByteString (b64TextDecodeBs)
-import Constellation.Util.Either (fromShowRight, flattenEithers)
+import Constellation.Util.Either (fromShowRight, flattenEithers, maybeToEitherT)
 import Constellation.Util.Lockable (promptingUnlock)
 
 newKeyPair :: IO (PublicKey, Box.SecretKey)
@@ -25,15 +25,12 @@ loadKeyPair :: (FilePath, FilePath)
             -> IO (Either String (PublicKey, Box.SecretKey))
 loadKeyPair (pubPath, privPath) = runEitherT $ do
     pubBs <- EitherT $ b64TextDecodeBs <$> readFileUtf8 pubPath
-    pub <- hoistMaybe "loadKeyPair: Failed to mkPublicKey" (mkPublicKey pubBs)
+    pub <- maybeToEitherT "loadKeyPair: Failed to mkPublicKey"
+                          (mkPublicKey pubBs)
     locked <- EitherT $ AE.eitherDecode' . fromStrict <$> readFile privPath
     liftIO $ putStrLn ("Unlocking " ++ privPath)
     privBs <- EitherT $ promptingUnlock locked
-    (pub,) <$> hoistMaybe "Failed to S.encode privBs" (S.decode privBs)
-
-  where
-    hoistMaybe :: Monad m => e -> Maybe a -> EitherT e m a
-    hoistMaybe err may = hoistEither $ maybe (Left err) Right may
+    (pub,) <$> maybeToEitherT "Failed to S.encode privBs" (S.decode privBs)
 
 loadKeyPairs :: [(FilePath, FilePath)]
              -> IO (Either String [(PublicKey, Box.SecretKey)])
