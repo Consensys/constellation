867be1c80a1c7ef5b00ea6b318bb0bfc1cbe87d7
Patrick Mylund Nielsen <patrick@patrickmn.com>
2017-05-31 22:36:30 -0400

    Add scripts/db-list-keys.py to aid with troubleshooting

---
+++ scripts/db-list-keys.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import base64
+import dbhash
+import sys
+
+def main():
+    if len(sys.argv) == 1:
+        usage()
+        return
+    for k, v in dbhash.open(sys.argv[1]).iteritems():
+        print k
+
+def usage():
+    print("Usage: {} <path to .db file, ...>".format(sys.argv[0]))
+
+if __name__ == "__main__":
+    main()
