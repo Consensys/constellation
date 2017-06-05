f2445fd61858fe56bc4381dc234d7ca3d11a98f5
Brian Schroeder <bts@gmail.com>
2017-06-05 16:41:51 -0400

    Enclave.Key: Expose key encoding functions
    This makes it easier to use constellation as a library (vs executable).

--- Constellation/Enclave/Key.hs
+++ Constellation/Enclave/Key.hs
@@ -11,17 +11,33 @@ import Control.Monad.Trans.Either (EitherT(EitherT), runEitherT)
 import qualified Crypto.Saltine.Class as S
 import qualified Crypto.Saltine.Core.Box as Box
 import qualified Data.Aeson as AE
+import qualified Data.ByteString.Base64.Lazy as B64L
+import qualified Data.ByteString.Lazy as BL
 import qualified Data.Text as T
 
-import Constellation.Enclave.Types (PublicKey(PublicKey), mkPublicKey)
+import Constellation.Enclave.Types
+    (PublicKey(PublicKey, unPublicKey), mkPublicKey)
 import Constellation.Util.ByteString (b64TextDecodeBs)
 import Constellation.Util.Either (fromShowRight, flattenEithers, maybeToEitherT)
-import Constellation.Util.Lockable (unlock, promptingUnlock)
+import Constellation.Util.Lockable
+    (Lockable(Unlocked), lock, promptingUnlock, unlock)
 
 newKeyPair :: IO (PublicKey, Box.SecretKey)
 newKeyPair = do
     (priv, pub) <- Box.newKeypair
     return (PublicKey pub, priv)
+
+b64EncodePublicKey :: PublicKey -> BL.ByteString
+b64EncodePublicKey = B64L.encode . BL.fromStrict . S.encode . unPublicKey
+
+-- | Optionally takes a password to lock the private key.
+jsonEncodePrivateKey :: Maybe String -> Box.SecretKey -> IO BL.ByteString
+jsonEncodePrivateKey mpwd priv = AE.encode <$> mkLockable
+  where
+    mkLockable = case mpwd of
+        Nothing  -> return $ Unlocked (S.encode priv)
+        Just ""  -> return $ Unlocked (S.encode priv)
+        Just pwd -> lock pwd (S.encode priv)
 
 loadKeyPair :: (FilePath, FilePath, Maybe String)
             -> IO (Either String (PublicKey, Box.SecretKey))
