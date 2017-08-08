c8dff06c1381e555a27765ddf9733feb29127914
Patrick Mylund Nielsen <patrick@patrickmn.com>
2017-08-08 10:31:05 -0400

    Enclave.Payload: Fix safeBeforeNM error message (shouldn't say 'encrypt')

--- Constellation/Enclave/Payload.hs
+++ Constellation/Enclave/Payload.hs
@@ -53,7 +53,7 @@ encrypt pl sender pk rcpts = encrypt' pl sender cks
 
 safeBeforeNM :: Box.PublicKey -> Box.SecretKey -> Box.PublicKey -> Box.CombinedKey
 safeBeforeNM sender pk rcpt
-    | sender == rcpt = error "encrypt: Sender cannot be a recipient"
+    | sender == rcpt = error "safeBeforeNM: Sender cannot be a recipient"
     | otherwise      = Box.beforeNM pk rcpt
 
 encrypt' :: ByteString
