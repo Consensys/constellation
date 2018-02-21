4c94d17b56271324311505a889a0977fc60961ee
Patrick Mylund Nielsen <patrick@patrickmn.com>
2018-02-21 16:15:22 -0500

    Node.Main: Bump version 0.3.1 -> 0.3.2

--- Constellation/Node/Main.hs
+++ Constellation/Node/Main.hs
@@ -68,7 +68,7 @@ import Constellation.Util.Logging (debugf', logf', warnf', errorf')
 import Constellation.Util.Tls (newSelfSignedCertificate)
 
 version :: Text
-version = "0.3.1"
+version = "0.3.2"
 
 defaultMain :: IO ()
 defaultMain = do
